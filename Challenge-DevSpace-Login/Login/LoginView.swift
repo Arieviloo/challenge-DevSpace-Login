765715bd502f3a40c1665b74ad599a5d81b62822
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-08-29 15:13:03 -0300

    login-view

--- Challenge-DevSpace-Login/Login/LoginView.swift
+++ Challenge-DevSpace-Login/Login/LoginView.swift
@@ -19,7 +19,6 @@ class LoginView: UIView {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.clipsToBounds = true
         $0.layer.cornerRadius = 20
-//        $0.frame = CGRect(x: 0, y: 0, width: frame.width, height: 100)
         $0.backgroundColor = .white
         return $0
     }(UIView())
@@ -27,24 +26,76 @@ class LoginView: UIView {
     lazy var signUpLabel: UILabel = {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.text = "Sign Up"
+        $0.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        $0.textColor = UIColor(red: 50/255, green: 59/255, blue: 142/255, alpha: 1.0)
         return $0
     }(UILabel())
     
     lazy var fullNameLabel: UILabel = {
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.text = "Full Name"
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .bold)
         return $0
     }(UILabel())
     
     lazy var fullNameTextField: UITextField = {
         $0.translatesAutoresizingMaskIntoConstraints = false
-        $0.backgroundColor = .lightGray
+        $0.backgroundColor = UIColor(red: 239/255, green: 239/255, blue: 253/255, alpha: 1.0)
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 8
+        $0.placeholder = "Peter Parker"
+        $0.autocorrectionType = .no
+        $0.keyboardType = .default
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 40))
+        $0.leftViewMode = .always
         return $0
     }(UITextField())
     
+    lazy var emailLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "Email"
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        return $0
+    }(UILabel())
+    
+    lazy var emailTextField: UITextField = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = UIColor(red: 239/255, green: 239/255, blue: 253/255, alpha: 1.0)
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 8
+        $0.placeholder = "peterparker@gmail.com"
+        $0.autocorrectionType = .no
+        $0.keyboardType = .emailAddress
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 40))
+        $0.leftViewMode = .always
+        return $0
+    }(UITextField())
+    
+    lazy var passwordLabel: UILabel = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.text = "Password"
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        return $0
+    }(UILabel())
+    
+    lazy var passwordTextField: UITextField = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = UIColor(red: 239/255, green: 239/255, blue: 253/255, alpha: 1.0)
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 8
+        $0.placeholder = "*******"
+        $0.autocorrectionType = .no
+        $0.keyboardType = .default
+        $0.isSecureTextEntry = true
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 40))
+        $0.leftViewMode = .always
+        return $0
+    }(UITextField())
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .orange
+        backgroundColor = UIColor(red: 50/255, green: 59/255, blue: 142/255, alpha: 1.0)
         configView()
         configConstraints()
     }
@@ -59,6 +110,10 @@ class LoginView: UIView {
         backgroundView.addSubview(signUpLabel)
         backgroundView.addSubview(fullNameLabel)
         backgroundView.addSubview(fullNameTextField)
+        backgroundView.addSubview(emailLabel)
+        backgroundView.addSubview(emailTextField)
+        backgroundView.addSubview(passwordLabel)
+        backgroundView.addSubview(passwordTextField)
     }
     
     private func configConstraints() {
@@ -84,10 +139,25 @@ class LoginView: UIView {
             fullNameTextField.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 5),
             fullNameTextField.leadingAnchor.constraint(equalTo: signUpLabel.leadingAnchor),
             fullNameTextField.trailingAnchor.constraint(equalTo: signUpLabel.trailingAnchor),
-            fullNameTextField.heightAnchor.constraint(equalToConstant: 40)
+            fullNameTextField.heightAnchor.constraint(equalToConstant: 40),
             
+            emailLabel.topAnchor.constraint(equalTo: fullNameTextField.bottomAnchor, constant: 20),
+            emailLabel.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            emailLabel.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor),
             
+            emailTextField.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 5),
+            emailTextField.leadingAnchor.constraint(equalTo: fullNameTextField.leadingAnchor),
+            emailTextField.trailingAnchor.constraint(equalTo: fullNameTextField.trailingAnchor),
+            emailTextField.heightAnchor.constraint(equalTo: fullNameTextField.heightAnchor),
             
+            passwordLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordLabel.leadingAnchor.constraint(equalTo: fullNameLabel.leadingAnchor),
+            passwordLabel.trailingAnchor.constraint(equalTo: fullNameLabel.trailingAnchor),
+            
+            passwordTextField.topAnchor.constraint(equalTo: passwordLabel.bottomAnchor, constant: 5),
+            passwordTextField.leadingAnchor.constraint(equalTo: fullNameTextField.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: fullNameTextField.trailingAnchor),
+            passwordTextField.heightAnchor.constraint(equalTo: fullNameTextField.heightAnchor)
             
         ])
     }
