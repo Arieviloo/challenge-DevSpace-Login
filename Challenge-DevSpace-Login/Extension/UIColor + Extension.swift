38d8c54b896f53a153dd2008b7293edbf0851a37
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-08-30 11:22:34 -0300

    adjustment

---
+++ Challenge-DevSpace-Login/Extension/UIColor + Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor + Extension.swift
+//  Challenge-DevSpace-Login
+//
+//  Created by Jadië Oliveira on 30/08/23.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primaryColor = UIColor(red: 50/255, green: 59/255, blue: 142/255, alpha: 1.0)
+    static let secondaryColor = UIColor(red: 239/255, green: 239/255, blue: 253/255, alpha: 1.0)
+}
